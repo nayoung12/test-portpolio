8f09cb70cbe362a982bde48914421bd52be95559
nayoung12 <skduddl200@gmail.com>
2024-09-22 23:38:17 +0900

    2024-09-22 commit

--- js/script.js
+++ js/script.js
@@ -8,3 +8,24 @@ function toggleSubmenu(menuId) {
         submenu.style.display = "block";
     }
 }
+
+/*
+document.addEventListener("DOMContentLoaded", function() {
+    // 모든 ul 태그에 대해 클릭 이벤트를 추가
+    const menuLists = document.querySelectorAll("ul");
+
+    menuLists.forEach(function(menu) {
+        menu.addEventListener("click", function() {
+            // <li> 요소를 토글하는 기능
+            const items = this.querySelectorAll("li");
+            items.forEach(function(item) {
+                if (item.style.display === "block") {
+                    item.style.display = "none"; // 숨기기
+                } else {
+                    item.style.display = "block"; // 보이기
+                }
+            });
+        });
+    });
+});
+ */
